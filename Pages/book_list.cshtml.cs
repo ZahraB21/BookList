b2ddf05a19239825b7dfdda2ec6c53c8eb0be4b8
Zahra <zbehfarshad@gmail.com>
2020-12-17 23:06:24 -0800

    Adding two pages: Books and Book-List to the nav of the layout

---
+++ Pages/book_list.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages
+{
+    public class book_listModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
